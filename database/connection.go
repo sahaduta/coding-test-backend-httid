d2412844c2f565ac70290f775abe78a9f1f59266
sahaduta <sahadutategar@gmail.com>
2024-02-17 17:25:16 +0700

    feat: added database connection

---
+++ database/connection.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewConn() (*gorm.DB, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s application_name=coding_test sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: nil,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, err
+}
